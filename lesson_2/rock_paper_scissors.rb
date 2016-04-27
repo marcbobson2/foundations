bba1c921a3dfbf5b9cb63c2c1cc305a799af2eaa
Marc Davis <marcbobson@gmail.com>
2016-04-27 00:21:29 +0000

    this is a test

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,21 +1,30 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors lizard spock).freeze
 
-def prompt(message)
-  puts("=> #{message}")
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'spock' && second == "scissors") ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'paper' && second == 'spock')
 end
 
 def display_results(player, computer)
-  if (player == 'rock'  && computer == 'scissors') ||
-    (player == 'paper' && computer == 'rock') ||
-    (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("you won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-    (player == 'paper'  && computer == 'scissors') ||
-    (player == 'scissors' && computer == 'rock')
-    prompt("computer wins!")
+  elsif win?(computer, player)
+    prompt("computer won!")
   else
     prompt("its a tie!")
   end
+end
+
+def prompt(message)
+  puts("=> #{message}")
 end
 
 loop do
@@ -23,11 +32,8 @@ loop do
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
     choice = gets.chomp
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("Thats not a valid choice.")
-    end
+    break if VALID_CHOICES.include?(choice)
+    prompt("Thats not a valid choice.")
   end
 
   computer_choice = VALID_CHOICES.sample
@@ -37,7 +43,7 @@ loop do
   display_results(choice, computer_choice)
 
   prompt("Do you want to play again?")
-  answer=gets.chomp
+  answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
