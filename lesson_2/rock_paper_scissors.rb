7b6472a8af8a0d9a6e530f7fd141a31749faa4d1
Marc Davis <marcbobson@gmail.com>
2016-04-30 15:27:55 +0000

    latest version

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,3 +1,4 @@
+NUM_ROUNDS = 5
 DISPLAY_CHOICES = %w(rock paper scissors lizard spock).freeze
 VALID_CHOICES = {
   "rock" => %w(r rock),
@@ -14,6 +15,10 @@ BEATS = {
   'lizard' => %w(paper spock),
   'spock' => %w(rock scissors)
 }.freeze
+
+def clear_screen
+  system('clear') || system('cls')
+end
 
 def win?(first, second)
   BEATS[first].include?(second)
@@ -44,37 +49,38 @@ end
 computer_score = 0
 human_score = 0
 
-loop do # outer_loop
+loop do 
   choice_found = []
   choice = ''
-  loop do # gather choice, validate it, and provide error msgs for invalid selections
+  
+  loop do 
     prompt("Choose one: #{DISPLAY_CHOICES.join(', ')}")
     choice = gets.chomp
-    # now loop through array and look for whole matches or first char matches
+    clear_screen
     choice_found = []
-    VALID_CHOICES.each do |key, value|
-      # now search the value array to see if choice is contained
-      choice_found << key if value.include?(choice)
-    end # end of compare_choice_to_valid_choices
-    # now validate findings
+    VALID_CHOICES.each do |standard_spelling_of_choice, variant_spelling_of_choice|
+      choice_found << standard_spelling_of_choice if variant_spelling_of_choice.include?(choice.downcase)
+    end 
+    
     break if choice_found.length == 1
 
-    puts "Based on your choice: #{choice}, it is unclear which of the following you meant: #{choice_found}. " \
-          "Please retry and type in the full word of your desired choice." if choice_found.length > 1
-    puts "Your input of #{choice} was invalid.  Please try again!" if choice_found.empty?
-  end # loop..do
+    puts "Based on your choice: #{choice}, it is unclear which of the following you meant: #{choice_found}.\n" \
+          "Please retry and type in the full word of your desired choice.\n\n" if choice_found.length > 1
+    puts "Your input of #{choice} was invalid.  Please try again!\n\n" if choice_found.empty?
+  end 
 
   computer_move = DISPLAY_CHOICES.sample
   puts "You chose #{choice_found[0]} and the computer chose #{computer_move}."
   display_results(choice_found[0], computer_move)
+  
   if win?(choice_found[0], computer_move)
     human_score += 1
   elsif win?(computer_move, choice_found[0])
     computer_score += 1
   end
 
-  puts "Your score: #{human_score} versus computer's score: #{computer_score}.  First to 5 wins!"
-  break if human_score == 5 || computer_score == 5
-end # play_again?
+  puts "\nYour score: #{human_score} versus computer's score: #{computer_score}.  First to #{NUM_ROUNDS} wins!\n\n"
+  break if human_score == NUM_ROUNDS || computer_score == NUM_ROUNDS
+end
 
 display_winner(human_score)
